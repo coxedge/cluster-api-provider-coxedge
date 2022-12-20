18a6f45a637353fd0921080bd931e29716e2a9d4
Matthew Mattox <mmattox@support.tools>
2022-12-20 01:51:24 -0600

    Switching to build number instead of latest

--- scripts/drone-build.sh
+++ scripts/drone-build.sh
@@ -4,7 +4,7 @@ docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD harbor.coxedgecomputing.com
 export REGISTRY=harbor.coxedgecomputing.com
 if [ -z "$DRONE_TAG" ];
 then
-  export DRONE_TAG=latest
+  export DRONE_TAG=$DRONE_BUILD_NUMBER
 fi
 export IMAGE_NAME=coxedge/cluster-api-cox-controller:${DRONE_TAG}
 make docker-build && make docker-push
